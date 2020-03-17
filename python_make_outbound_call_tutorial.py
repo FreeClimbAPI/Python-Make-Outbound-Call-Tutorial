7dbc91285e56489ad027e05eaad812af42c6ba80
Francis San Filippo <fsanfilipp@vailsys.com>
2020-03-17 11:38:01 -0500

    update for SDK v1.0.1

--- python_make_outbound_call_tutorial.py
+++ python_make_outbound_call_tutorial.py
@@ -4,6 +4,7 @@ import freeclimb
 import os
 import json
 from flask import Flask, request
+from freeclimb import percl_to_json
 
 configuration = freeclimb.Configuration()
 # Configure HTTP basic authorization: fc
@@ -21,23 +22,24 @@ app = Flask(__name__)
 @app.route('/sendCall', methods=['POST'])
 def sendCall():
     if request.method == 'POST':
-        call_request = freeclimb.MakeCallRequest(_from=YOUR_FREECLIMB_NUMBER, to=YOUR_VERIFIED_NUMBER, application_id=YOUR_APP_ID)
+        call_request = freeclimb.MakeCallRequest(
+            _from='+19193468860', to='+17733133558', application_id='AP5991fbadecfdcd227d326b23a3d498efca14d387')
         api_instance.make_a_call(make_call_request=call_request)
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 # Specify this route with 'CALL CONNECT URL' in App Config
 @app.route('/callConnect', methods=['POST'])
 def callConnect():
     if request.method == 'POST':
-        percl = freeclimb.PerCLScript()
+        script = freeclimb.PerclScript(commands=[])
 
-        percl.append(freeclimb.Say(text="Hello. Welcome to FreeClimb's outbound call tutorial."))
-        percl.append(freeclimb.Pause(length=1000))
-        percl.append(freeclimb.Say(text="Goodbye."))
-        return json.dumps(percl)
+        script.commands.append(freeclimb.Say(
+            text="Hello. Welcome to FreeClimb's outbound call tutorial."))
+        script.commands.append(freeclimb.Pause(length=1000))
+        script.commands.append(freeclimb.Say(text="Goodbye."))
+        return percl_to_json(script)
 
 # Specify this route with 'STATUS CALLBACK URL' in App Config
 @app.route('/status', methods=['POST'])
 def status():
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
